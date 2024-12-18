0b102a2ffd24e42d1f8b8ca9ecba3c7cfa5839dc
ahmedwael <ahmedwael99104@gmail.com>
2024-12-18 17:48:44 +0200

    create reminder view body

--- lib/features/tasks/presentation/views/reminder_view.dart
+++ lib/features/tasks/presentation/views/reminder_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:tasks_app/features/tasks/presentation/views/widgets/reminder_view_body.dart';
 
 class ReminderView extends StatelessWidget {
   const ReminderView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: ReminderViewBody(),
+    );
   }
 }
