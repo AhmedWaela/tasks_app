496c4b2d4701c881bdb262754945f5d0d99532f9
ahmedwael <ahmedwael99104@gmail.com>
2024-12-18 17:20:05 +0200

    convert object to json

--- lib/features/tasks/data/models/task_model.dart
+++ lib/features/tasks/data/models/task_model.dart
@@ -3,14 +3,25 @@ import 'package:flutter/material.dart';
 class TaskModel {
   final int taskId;
   final String title;
-  final String description;
+  final String? description;
   final DateTime dateTime;
   final TimeOfDay timeOfDay;
 
-  TaskModel(
-      {required this.taskId,
-      required this.title,
-      required this.description,
-      required this.dateTime,
-      required this.timeOfDay});
+  TaskModel({
+    required this.taskId,
+    required this.title,
+    this.description,
+    required this.dateTime,
+    required this.timeOfDay,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'taskId': taskId,
+      'title': title,
+      if (description != null) 'description': description,
+      'dateTime': dateTime,
+      'timeOfDay': timeOfDay
+    };
+  }
 }
