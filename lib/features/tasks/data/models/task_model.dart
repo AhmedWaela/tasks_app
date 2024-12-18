d90d1c165d44c835dba058c6a8139f4107aaa642
ahmedwael <ahmedwael99104@gmail.com>
2024-12-18 17:17:45 +0200

    add task attributs

--- lib/features/tasks/data/models/task_model.dart
+++ lib/features/tasks/data/models/task_model.dart
@@ -1,1 +1,16 @@
-class TaskModel {}
+import 'package:flutter/material.dart';
+
+class TaskModel {
+  final int taskId;
+  final String title;
+  final String description;
+  final DateTime dateTime;
+  final TimeOfDay timeOfDay;
+
+  TaskModel(
+      {required this.taskId,
+      required this.title,
+      required this.description,
+      required this.dateTime,
+      required this.timeOfDay});
+}
