202365eb350e3b35084d3e198e95ebc1ea045d6c
ahmedwael <ahmedwael99104@gmail.com>
2024-12-18 19:19:43 +0200

    add time pick dialog

--- lib/features/tasks/presentation/views/widgets/add_reminder_view_body.dart
+++ lib/features/tasks/presentation/views/widgets/add_reminder_view_body.dart
@@ -1,72 +1,89 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide TimePickerDialog;
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tasks_app/features/tasks/presentation/views/widgets/custom_text_field.dart';
+import 'package:tasks_app/features/tasks/presentation/views/widgets/custom_time_picker_dialog.dart';
 
-class AddReminderViewBody extends StatelessWidget {
+class AddReminderViewBody extends StatefulWidget {
   const AddReminderViewBody({super.key});
 
+  @override
+  State<AddReminderViewBody> createState() => _AddReminderViewBodyState();
+}
+
+class _AddReminderViewBodyState extends State<AddReminderViewBody> {
+  TimeOfDay timeOfDay = TimeOfDay.now();
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: MediaQuery.sizeOf(context).height * 0.01,
-            ),
-            CustomTextField(
-              controller: TextEditingController(),
-              label: 'Title',
-            ),
-            CustomTextField(
-              controller: TextEditingController(),
-              label: 'Description',
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              height: 60, // Adjusted height for better alignment
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8), // Adds rounded corners
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.01,
               ),
-              child: Row(
-                children: [
-                  Column(
-                    mainAxisAlignment:
-                        MainAxisAlignment.center, // Align vertically
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        'Enter Date',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 4), // Adds spacing between lines
-                      Text(
-                        'yy/mm',
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  SvgPicture.asset(
-                    'assets/images/Icon (1).svg',
-                    height: 30,
-                  ),
-                ],
+              CustomTextField(
+                controller: TextEditingController(),
+                label: 'Title',
               ),
-            ),
-            const Divider(
-              color: Colors.green,
-              thickness: 1.5, // Adjusted thickness for better visibility
-            ),
-          ],
+              CustomTextField(
+                controller: TextEditingController(),
+                label: 'Description',
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                height: 60, // Adjusted height for better alignment
+                decoration: BoxDecoration(
+                  borderRadius:
+                      BorderRadius.circular(8), // Adds rounded corners
+                ),
+                child: Row(
+                  children: [
+                    Column(
+                      mainAxisAlignment:
+                          MainAxisAlignment.center, // Align vertically
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Enter Date',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 4), // Adds spacing between lines
+                        Text(
+                          'yy/mm',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    SvgPicture.asset(
+                      'assets/images/Icon (1).svg',
+                      height: 30,
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(
+                color: Colors.green,
+                thickness: 1.5, // Adjusted thickness for better visibility
+              ),
+              TimePickerDialog(
+                initialTime: TimeOfDay.now(),
+                onSelected: (time) {
+                  timeOfDay = time;
+                  setState(() {});
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
