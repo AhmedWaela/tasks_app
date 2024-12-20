f4945df1183c0d911edc86df9585d6dd8821b008
ahmedwael <ahmedwael99104@gmail.com>
2024-12-20 16:06:48 +0200

    add action buttons for notification and icon

--- lib/core/services/flutter_local_notifications_service.dart
+++ lib/core/services/flutter_local_notifications_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:tasks_app/features/tasks/presentation/views/add_reminder_view.dart';
 import 'package:tasks_app/main.dart';
@@ -89,17 +90,76 @@ class FlutterLocalNotificationsService {
 
   static Future<void> showScheduleNotification(
       int id, String title, String description, DateTime scheduledTime) async {
+    final ByteData byteData = await rootBundle.load('assets/images/pngegg.png');
+    final Uint8List imageBytes = byteData.buffer.asUint8List();
     NotificationDetails notificationDetails = NotificationDetails(
       android: AndroidNotificationDetails(
+        // channel Id => A unique identifier for a notification channel
+        // Purpose => It is used internally by android to identify the channel. Multiple
+        // notification can be grouped under the same channel id
         'Schedule Notification Channel Id',
+        // A human readable name for the notification channel
+        // purpose => this name is displayed in the system settings under the app's notification
+        // settings. it helps users identfy what kind of notifications they are customizing
         'Schedule Notification Channel',
+        // تحدد مدي اولويه الاشعار بالنسبة للمستخدم
         priority: Priority.high,
+        // to appear notifications as a heads up on screen
+        // it works with priority to control on notification behavior
         importance: Importance.max,
+        // Explain exactly what notifications are for
+        // Highlights the type of information the user will receive (events,tasks,appointments)
+        // Make the app user friendly
+        channelDescription:
+            'This channel sends reminders and updates for your schedule events, tasks,and appointments to keep you on truck',
+        // The icon that should be used when displaying the notification.
+        icon: 'notifications',
+        // Specifies if the notification should automatically dismissed upon tapping on it.
+        autoCancel: false,
+        subText: 'Next Challange',
+
+        actions: [
+          AndroidNotificationAction(
+            '0',
+            'Clear',
+            cancelNotification: true,
+          ),
+          AndroidNotificationAction(
+            '1',
+            'Schedule Task for another time',
+            cancelNotification: false,
+            titleColor: Colors.green,
+            showsUserInterface: true,
+            inputs: [
+              AndroidNotificationActionInput(
+                allowedMimeTypes: {
+                  'After hour',
+                  'After day',
+                  'After week',
+                  'After Month',
+                },
+                choices: [
+                  'After hour',
+                  'After day',
+                  'After week',
+                  'After Month',
+                ],
+              )
+            ],
+          )
+        ],
         sound:
             RawResourceAndroidNotificationSound('sound.mp3'.split('.').first),
+        styleInformation:
+            BigPictureStyleInformation(ByteArrayAndroidBitmap(imageBytes)),
+        largeIcon: ByteArrayAndroidBitmap(imageBytes),
       ),
       iOS: DarwinNotificationDetails(),
     );
+
+    Future.delayed(const Duration(hours: 2), () async {
+      await cancelNotification(id);
+    });
 
     try {
       // Get the timezone and set the local timezone
